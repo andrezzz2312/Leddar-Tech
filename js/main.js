d58a147875bf60b654543f91b496fe981910a85e
andrezzz2312 <andrezzz2312@gmail.com>
2023-01-04 16:53:09 -0500

    test 3

--- js/main.js
+++ js/main.js
@@ -179,7 +179,6 @@ const buttonContent = [
 // Display fullscreen button
 if (!isMobileDevice) {
 	fullscreen_button.style.display = 'none'
-	alert('asd')
 } else {
 	if (isIOS) {
 		fullscreen_button.style.display = 'none'
@@ -579,6 +578,20 @@ window.addEventListener('resize', function () {
 
 		if (!mainButtons.classList.contains('disabled')) {
 			ArreglarLineas()
+		}
+	}
+	if (window.matchMedia('(max-width: 520px)').matches) {
+		if (window.matchMedia('(orientation: portrait)').matches) {
+			warningText.innerHTML =
+				' Use the device in landscape mode in order to properly use this website'
+			warning.style.opacity = '1'
+			warning.style.zIndex = '300'
+		}
+	} else {
+		if (window.matchMedia('(orientation: landscape)').matches) {
+			warning.style.opacity = '0'
+			warning.style.zIndex = '-100'
+			window.scrollTo(0, document.body.scrollHeight)
 		}
 	}
 })
