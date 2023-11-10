233ae2af31324de8656ae753e44c31e8943becb8
andrezzz2312 <andrezzz2312@gmail.com>
2023-11-10 11:58:32 -0500

    final

--- js/environment.js
+++ js/environment.js
@@ -2,13 +2,22 @@ const e_buttons = document.querySelectorAll('.e_button')
 const e_buttonGroup = document.querySelector('.e_buttonGroup')
 const e_subButton = document.querySelectorAll('.e_subButton')
 const e_video = document.querySelector('.e_video')
-
 const eVideo = document.querySelector('.e_video')
 const play = document.querySelector('.fa-play')
 
-let perHArray = ['LVF-E', 'LVF-H', 'LVS-2+', 'LVP']
+const titles = [
+	`Products`,
+	`Operating\nin all\nconditions`,
+	`Operating with\nall sensor\nmodals`,
+	`Off-road`,
+]
+
+e_buttons.forEach((e, i) => {
+	e.textContent = titles[i]
+})
+let productsArray = ['LVF-E', 'LVF-H', 'LVS-2+', 'LVP']
 e_subButton.forEach((e, i) => {
-	e.textContent = perHArray[i]
+	e.textContent = productsArray[i]
 })
 
 e_buttons.forEach((buttonElement, i) => {
@@ -25,68 +34,68 @@ e_buttons.forEach((buttonElement, i) => {
 		})
 
 		switch (e.currentTarget.dataset.id) {
-			case 'perH':
-				perHArray = ['LVF-E', 'LVF-H', 'LVS-2+', 'LVP']
-				const perHid = ['3drE', 'perC', 'lanD', 'addF']
-				perHid.forEach((e, i) => {
-					perHid[i] = 'perH' + e
+			case 'prod':
+				productsArray = ['LVF-E', 'LVF-H', 'LVS-2+', 'LVP']
+				const prodid = ['LVF-E', 'LVF-H', 'LVS-2+', 'LVP']
+				prodid.forEach((e, i) => {
+					prodid[i] = 'prod' + e
 				})
 				e_subButton.forEach((subButtonElement, i) => {
-					subButtonElement.dataset.id = perHid[i]
-					subButtonElement.textContent = perHArray[i]
+					subButtonElement.dataset.id = prodid[i]
+					subButtonElement.textContent = productsArray[i]
 				})
 				break
 
-			case 'adaS':
-				const adaSArray = [
+			case 'opeC':
+				const opeCArray = [
 					'Various enviromental\nConditions',
 					'Crowded urban\nscenes',
 					'Crowded highway\nscenes',
 					'Degraded, conflicting,\nand failing sensors',
 				]
-				const adaSid = ['objS', 'occO', 'lanD', 'addF']
-				adaSid.forEach((e, i) => {
-					adaSid[i] = 'adaS' + e
+				const opeCid = ['varE', 'croU', 'croH', 'degC']
+				opeCid.forEach((e, i) => {
+					opeCid[i] = 'opeC' + e
 				})
 				e_subButton.forEach((e, i) => {
-					e.dataset.id = adaSid[i]
-					e.textContent = adaSArray[i]
+					e.dataset.id = opeCid[i]
+					e.textContent = opeCArray[i]
 				})
 				break
 
-			case 'opeE':
-				const opeEArray = [
+			case 'opeS':
+				const opeSArray = [
 					'Radar-Camera Fusion',
 					'LiDAR-Camera Fusion',
 					'LiDAR-Camera-Radar Fusion',
 					'Single modality',
 				]
-				const opeEid = ['opeB', 'opeR', 'faiC', 'opeD']
-				opeEid.forEach((e, i) => {
-					opeEid[i] = 'opeE' + e
+				const opeSid = ['radC', 'lidCF', 'lidCR', 'sinM']
+				opeSid.forEach((e, i) => {
+					opeSid[i] = 'opeS' + e
 				})
 
 				e_subButton.forEach((e, i) => {
-					e.dataset.id = opeEid[i]
-					e.textContent = opeEArray[i]
+					e.dataset.id = opeSid[i]
+					e.textContent = opeSArray[i]
 				})
 				break
 
-			case 'ledO':
-				const ledOArray = [
+			case 'offR':
+				const offRArray = [
 					'Harsh environment -\nDust & Dirt',
 					'LLF outperforms\nsingle modality',
 					'Failing and \ndegraded sensors',
 					'Unstructured\nenvironment',
 				]
-				const ledOid = ['envM', '3dT', 'opeD', 'detA']
-				ledOid.forEach((e, i) => {
-					ledOid[i] = 'ledO' + e
+				const offRid = ['harE', 'llfO', 'faiD', 'unsE']
+				offRid.forEach((e, i) => {
+					offRid[i] = 'offR' + e
 				})
 				e_subButton.forEach((e, i) => {
-					e.dataset.id = ledOid[i]
+					e.dataset.id = offRid[i]
 					e.classList.add('e_buttonPurple')
-					e.textContent = ledOArray[i]
+					e.textContent = offRArray[i]
 				})
 				break
 			default:
@@ -117,53 +126,52 @@ e_subButton.forEach((subButtonElement) => {
 		}
 		console.log(e.currentTarget.dataset.id)
 		switch (e.currentTarget.dataset.id) {
-			case 'perH3drE':
-				console.log('working')
+			case 'prodLVF-E':
 				e_video.src = './assets/environments/environmentalVideos/1.1.mp4'
 				break
-			case 'perHperC':
+			case 'prodLVF-H':
 				e_video.src = './assets/environments/environmentalVideos/1.2.mp4'
 				break
-			case 'perHlanD':
+			case 'prodLVS-2+':
 				e_video.src = './assets/environments/environmentalVideos/1.3.mp4'
 				break
-			case 'perHaddF':
+			case 'prodLVP':
 				e_video.src = './assets/environments/environmentalVideos/1.4.mp4'
 				break
-			case 'adaSobjS':
+			case 'opeCvarE':
 				e_video.src = './assets/environments/environmentalVideos/2.1.mp4'
 				break
-			case 'adaSoccO':
+			case 'opeCcroU':
 				e_video.src = './assets/environments/environmentalVideos/2.2.mp4'
 				break
-			case 'adaSlanD':
+			case 'opeCcroH':
 				e_video.src = './assets/environments/environmentalVideos/1.3.mp4'
 				break
-			case 'adaSaddF':
+			case 'opeCdegC':
 				e_video.src = './assets/environments/environmentalVideos/1.4.mp4'
 				break
-			case 'opeEopeB':
+			case 'opeSradC':
 				e_video.src = './assets/environments/environmentalVideos/3.1.mp4'
 				break
-			case 'opeEopeR':
+			case 'opeSlidCF':
 				e_video.src = './assets/environments/environmentalVideos/3.2.mp4'
 				break
-			case 'opeEfaiC':
+			case 'opeSlidCR':
 				e_video.src = './assets/environments/environmentalVideos/3.3.mp4'
 				break
-			case 'opeEopeD':
+			case 'opeSsinM':
 				e_video.src = './assets/environments/environmentalVideos/3.4.mov'
 				break
-			case 'ledOenvM':
+			case 'offRharE':
 				e_video.src = './assets/environments/environmentalVideos/4.1.mp4'
 				break
-			case 'ledO3dT':
+			case 'offRllfO':
 				e_video.src = './assets/environments/environmentalVideos/4.2.mp4'
 				break
-			case 'ledOopeD':
+			case 'offRfaiD':
 				e_video.src = './assets/environments/environmentalVideos/3.4.mov'
 				break
-			case 'ledOdetA':
+			case 'offRunsE':
 				e_video.src = './assets/environments/environmentalVideos/4.4.mp4'
 				break
 			default:
